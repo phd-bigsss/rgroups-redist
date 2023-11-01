5227baea2daba14e514b777e33290cbdf4f7567e
Julio César Iturra Sanhueza <julioiturrasanhueza@gmail.com>
2023-11-01 15:50:41 +0100

    update

--- procesamiento/prod_analysis-centrado.R
+++ procesamiento/prod_analysis-centrado.R
@@ -2,7 +2,7 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(dplyr,sjmisc,ggplot2,interplot,marginaleffects,sjlabelled,haven,stringr,ordinal,ggplot2)  
 rm(list=ls())
 load(here::here("input/data/proc/study1_country.RData"));df1 <- df2
-dfreg <- df1 %>% dplyr::select(redist,
+dfreg <- df1 %>% dplyr::select(redist=egal2,
                                class3_ser_gc,
                                class3_mid_gc,
                                class3_wor_gc,
@@ -46,7 +46,7 @@ dfreg <- df1 %>% dplyr::select(redist,
                                ttheilge1,
                                  WEIGHT,
                                  region,                              
-                                 country2=country3,
+                                 country2,
                                  country,
                                rgdpna,
                                oecd
@@ -57,6 +57,7 @@ mutate(logrgdpna=log(rgdpna),
   # mutate(homclass=group_center(homclass,country2))  %>%
   # filter(!country2 %in% c("TWN","CHN","JPN")) %>%
   # filter(oecd=="OECD" & country2!="SVN") %>%
+  filter(country2!="SVN") %>% 
   na.omit()
 
 base <- lmer(redist~1 + (1|country2),data=dfreg,weights = WEIGHT); icc1<- performance::icc(base)
@@ -68,8 +69,7 @@ rob2 <- update(rob1, . ~ . -(1|country2) + (homclass|country2))
 # anova(rob1,rob2)
 # Interactions segregation x class
 int_homo <- update(rob1, . ~ . +class3_mid_gc*homclass+class3_wor_gc*homclass)
-int_homo_country <- update(rob2, . ~ . -(1|country2) + (class3_mid_gc*homclass+class3_wor_gc*homclass|country2))
-models <- list(homclass,full1,rob1,int_homo,int_homo_country)
+models <- list(homclass,full1,rob1,int_homo)
 texreg::knitreg(models,stars = c(.001, .01, .05, .1),symbol = '\u2020')
 
   # Homoclass x Other Economic Inequality-----------------------------------------
@@ -90,8 +90,6 @@ models_m <- list(int_ginid,int_ginim,int_palma,int_pc10,int_pc50,int_d10d1,
                  int_gini_wiid,int_theil)
 
 texreg::knitreg(models_m,stars = c(.001, .01, .05, .1))
-
-
 interplot::interplot(m = int_ginim,var1 = "homclass",var2="gini_mkt",point = T)
 
 predval1 <- 
@@ -101,18 +99,10 @@ predval1 <-
   theme(legend.position = "right")
 
 
-
-broom_df<-
-broom.mixed::tidy(int_homo_country, effects = "ran_vals", conf.int = TRUE)
-
-broom_df
-
 plot_df <- 
 broom_df %>% 
   filter(term == "class3_mid_gc:homclass" | term =="homclass:class3_wor_gc") %>% 
   arrange(level) 
-
-
 ggplot(
   plot_df,
   aes(x = estimate, y = level ,group=term, fill=term)) +
@@ -121,3 +111,97 @@ ggplot(
   geom_vline(xintercept = 0) +
   scale_x_discrete() +
   facet_grid(~term)
+
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,sjmisc,ggplot2,interplot,marginaleffects,haven,ordinal,brms)  
+rm(list=ls())
+load(here::here("input/data/proc/study1_country.RData"));df1 <- df2
+
+df1$redistOR <- ordered(x = df1$redist)
+dfreg <- df1 %>% dplyr::select(redistOR,
+                               class3,
+                               homclass,
+                               Q03pcm,
+                               edyears,
+                               female,
+                               agenum,
+                               union,
+                               workst,
+                               WEIGHT,
+                               region,
+                               "gini_disp",
+                               "gini_mkt",
+                               rgdpna,
+                               oecd,
+                               country2,country
+) %>% 
+  mutate(logrgdpna=log(rgdpna),
+         edyears2=edyears^2,
+         age2=agenum^2) %>%
+  na.omit()
+
+# Models 
+base <- clm(redistOR~1 + factor(country2),data=dfreg,weights = WEIGHT)
+homclass<- update(base, . ~ . +homclass)
+full1 <- update(homclass, . ~ . + class3+female+agenum+age2)
+rob1 <- update(full1, . ~ . + edyears+ Q03pcm+union+workst)
+
+# Interactions segregation x class
+int_homo <- update(rob1, . ~ . +class3*homclass)
+models <- list(homclass,full1,rob1,int_homo)
+texreg::screenreg(models,omit.coef = "(country2)")
+
+# Create dummy dataset for predictions
+newdata = datagrid(
+  model = int_homo,
+  redistOR = dfreg$redistOR,
+  homclass = c(
+    mean(dfreg$homclass) - sd(dfreg$homclass),
+    mean(dfreg$homclass),
+    mean(dfreg$homclass) + sd(dfreg$homclass)
+  ),
+  class3 = dfreg$class3
+)
+
+p <- predictions(model = int_homo,by = c("homclass","class3"),newdata = newdata)
+
+p2 <- 
+p %>% 
+  filter(group %in% c(1,3,5) &
+           class3 %in% c("Service Class (I+II)","Working Class (V+VI+VII)"))
+
+p2$class3 <- factor(p2$class3,levels = c("Working Class (V+VI+VII)","Service Class (I+II)"))
+p2$group  <- factor(p2$group,labels = c("Strongly disagree",
+                                        "Disagree Neither \n agree nor disagree",
+                                        "Strongly agree"))
+
+p2 %>%
+  ggplot(aes(
+    x = class3,
+    y = estimate,
+    fill = factor(homclass)
+  )) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_errorbar(
+    aes(ymin = conf.low, ymax = conf.high),
+    width=.2,
+    linewidth = 1,
+    position = position_dodge(0.9)
+  ) +
+  facet_grid( ~ group) +
+  labs(title = "",
+       x = NULL,
+       y = "Predicted probability",
+       fill = "Homogeneity") +
+  scale_fill_manual(labels = c("- 1 SD", "Mean", "+ 1 SD"),
+                    values = RColorBrewer::brewer.pal(name = "Greys",n = 3)) +
+  scale_x_discrete(labels=c("Working Class","Service Class")) +
+  scale_y_continuous(n.breaks = 10) +
+  theme_bw() +
+  theme(legend.position = "bottom")
+  
+
+
+  
+  
